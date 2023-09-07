0de6f9b64d438389f8acb672fb069159ea1cf034
harkanni <harkanzyempire001@gmail.com>
2023-09-07 14:16:41 +0100

    slack api

---
+++ public/assets/index.js
@@ -0,0 +1,54 @@
+async function fetchUserInfo() {
+  // API endpoint URL
+  const apiUrl = `http://localhost:3000/server`;
+
+  // Make the API request using the Fetch API
+  await fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const weekday = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+
+      const {
+        userInfo: { user }
+      } = data;
+
+      console.log(user.real_name);
+
+      let slackDisplayImageId = document.querySelectorAll('[data-testid = slackDisplayImage]');
+      slackDisplayImageId.forEach((slackDisplayImage) => {
+         slackDisplayImage.src = user.profile.image_original
+         slackDisplayImage.alt = user.real_name
+      });
+
+      let slackUserNameId = document.querySelectorAll('[data-testid = slackUserName]');
+      slackUserNameId.forEach((slackUserTag) => (slackUserTag.innerHTML = `${user.real_name}`));
+
+      const d = new Date();
+      let dayOfTheWeek = weekday[d.getDay()];
+
+      let currentDayOfTheWeek = document.querySelector('[data-testid = currentDayOfTheWeek]');
+      currentDayOfTheWeek.innerHTML = `${dayOfTheWeek}`
+
+      let currentUTCTime = document.querySelector('[data-testid = currentUTCTime]');
+      currentUTCTime.innerHTML = `${d.toUTCString()}`
+
+      let githubURL = document.querySelector('[data-testid = githubURL]');
+      githubURL.href = `https://github.com/harkanni`
+    })
+    .catch((error) => {
+      console.error('Fetch Error:', error);
+    });
+
+  // UPDATE HTML PAGE;
+}
+
+let userInfo = fetchUserInfo();
+console.log(userInfo);
---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+
+import cors from 'cors'
+
+import dotenv from 'dotenv'
+
+import { fetchUserInfo } from './app.js';
+
+dotenv.config()
+// import { logger } from './app';
+
+
+// logger()
+
+const app = express();
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/', express.static('public'));
+
+app.get('/server', async (req, res) => {
+   console.log("Server called");
+   let userInfo = await fetchUserInfo()
+   res.json({message: 'Hello from server!', userInfo: userInfo});
+})
+
+
+app.listen(PORT, () => {
+   console.log(`Example app listening on port ${PORT}`)
+ })
